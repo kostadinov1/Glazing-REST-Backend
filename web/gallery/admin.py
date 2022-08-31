c61e7e6eec5b96a5f13880cb8ad349c7fe58838c
kostadinov1 <evgenikostadinov1987@gmail.com>
2022-08-31 17:17:46 +0300

    added gallery admins

--- web/gallery/admin.py
+++ web/gallery/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from web.gallery.models import Album, Image
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
--- web/gallery/models.py
+++ web/gallery/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Album(models.Model):
     name = models.CharField(max_length=50, default='Unknown', blank=True, null=True,)
     created_on = models.DateTimeField(auto_now_add=True)
+
 
 class Image(models.Model):
     image_file = models.ImageField(upload_to='')
